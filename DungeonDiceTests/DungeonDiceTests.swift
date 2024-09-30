bfe7456a07036ae9680d7272687e2b1805f3e238
anorazhu <zhusm@bc.edu>
2024-09-29 23:26:55 -0400

    Initial Commit

---
+++ DungeonDiceTests/DungeonDiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceTests.swift
+//  DungeonDiceTests
+//
+//  Created by Anora Zhu on 9/29/24.
+//
+
+import Testing
+@testable import DungeonDice
+
+struct DungeonDiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
